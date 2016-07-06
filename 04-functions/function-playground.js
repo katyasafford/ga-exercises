3753f80d5def83908b9808d8420e7e626f747ec4
Katya Safford <katerin2011@gmail.com>
2016-07-05 21:11:44 -0500

    lesson 4

---
+++ 04-functions/function-playground.js
@@ -0,0 +1,34 @@
+//speak("Hi!");
+
+// var spoken = speak("hi");
+// console.log(spoken);
+//
+// function speak(words) {
+//   console.log(words);
+//   return true;
+// }
+
+//function declaration
+// function add (a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(1, 2));
+
+//function expression
+// var speak = function(words) {
+//   return words
+// }
+
+// function greet(greeting) {
+//   return greeting;
+// };
+// console.log(greet('hi'));
+// var a = greet;
+// console.log(a('hi from a'));
+
+var symbol = "#";
+for (var  symbol; symbol.length <= 7; symbol += "#") {
+  console.log(symbol);
+  //symbol += symbol;
+}
